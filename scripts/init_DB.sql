53fac640a77c1c6bf8de7b4966ff3ac5005a9fd0
bhargavsai712 <bhargavsai712@gmail.com>
2025-11-17 15:07:52 +0530

    Update init_DB.sql

--- scripts/init_DB.sql
+++ scripts/init_DB.sql
@@ -3,8 +3,7 @@ CREATE DATABASE & SCHEMAS
 
 PURPOSE:
 Creates a database named 'DataWH' after checking if it already exists.
-If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas Within the database: '' bronze
-, 'silver','gold'
+If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas Within the database: 'bronze,'silver','gold'
 
 CAUTION:
 Running this script will drop entire database if already exists.
