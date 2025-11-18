8d758e1d74eb9a0e381147a00311e6bd75c3621f
bhargavsai712 <bhargavsai712@gmail.com>
2025-11-18 15:02:23 +0530

    Create check_silver_layer.sql

---
+++ tests/check_silver_layer.sql
@@ -0,0 +1,12 @@
+/*
+Quality Checks Silver layer
+
+Purpose:
+    This script performs various quality checks for data consistency, accuracy, 
+    and standardization across the 'silver' layer. It includes checks for:
+    - Null or duplicate primary keys.
+    - Unwanted spaces in string fields.
+    - Data standardization and consistency.
+    - Invalid date ranges.
+    - Data consistency between related fields.
+*/
